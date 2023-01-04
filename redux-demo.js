c12b7965e15dd3ca651c89815a08b5398879355f
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-04 10:49:58 +0530

    reduz create store

---
+++ redux-demo.js
@@ -0,0 +1,33 @@
+import redux from 'redux';
+
+
+const counterReducer = (previousState = {count: 0}, action)=> { // --> 2. reducer function.. having 2 parameter.. 1) previous state 2) action... 
+																//		  and reducer function always return a new updated state.
+	if (action.type === "increment") {
+		return {
+			count: previousState.count + 1
+		};
+	}								
+	if (action.type === "decrement") {
+		return {
+			count: previousState.count - 1
+		};
+	}								 
+	return previousState;
+};
+
+const store = redux.createStore(counterReducer);  //--> 1. creating redux store using createStore() function... which takes a reducer function as a parameter.
+console.log(store.getState());
+
+
+const counterSubscriber = ()=> {	// --> 4. subscriber function is the subscriber of the redux store.... 
+									//		  which wil use the component state which is stored. by using getStore() function.
+	const latestState = store.getState();
+	console.log(latestState);
+}
+store.subscribe(counterSubscriber);
+
+
+
+store.dispatch({type: "increment"}) // --> 3. this function dispatch the action of reducer function...
+store.dispatch({type: "decrement"}) // --> 3. this function dispatch the action of reducer function...
